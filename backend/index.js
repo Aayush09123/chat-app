bcc2cb9c93177b6679a0fac17010cea84d234c34
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-23 18:17:42 +0530

    import authRouter in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 
+const port = process.env.PORT || 7000;
+
 const app = express();
 
-const port = process.env.PORT || 7000;
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
-})
+});
