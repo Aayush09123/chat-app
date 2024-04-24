c1ae5f9d0634bbacb60ca5bd477398b2508ad03e
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-24 13:05:37 +0530

    add auth controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) => {
+    console.log("Login user")
+}
+
+export const logout = (req, res) => {
+    console.log("Logout user")
+}
+
+export const signup = (req, res) => {
+    console.log("Signup user")
+}
