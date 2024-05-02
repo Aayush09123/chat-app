6bf4606d132554597e9c6dc2dcdfd0b0b5bee7d6
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-02 16:36:21 +0530

    add logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,3 +79,13 @@ export const login = async (req, res) => {
         res.status(500).json({ error: "Internal server error" })
     }
 }
+
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged out successfully" })
+    } catch (error) {
+        console.log("Error in login controller", error?.message);
+        res.status(500).json({ error: "Internal server error" })
+    }
+}
