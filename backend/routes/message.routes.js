e45f0c3854d350872fa0d9b82b0651878229ec70
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-11 15:33:48 +0530

    import sendMessage controller

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { sendMessage } from "../controllers/message.controller.js";
 
 const router = Router()
 
-router.route("/send/:id").post(sendMessage)
+router.route("/send/:id").post(sendMessage);
 
 export default router;
