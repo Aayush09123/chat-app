525256242161d864f120acda6fa56468fd8ceaab
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-11 14:00:49 +0530

    add a message route middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
+import messageRouter from "./routes/message.routes.js";
 import connectDB from "./db/index.js";
 
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json()) // to parse the incoming requests with JSON payloads
 
 app.use("/api/auth", authRouter);
+app.use("/api/messages", messageRouter)
 
 connectDB().then(() => {
     app.listen(port, () => {
