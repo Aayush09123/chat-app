0a1d21f8eb0f30479cabb1817899ba24964ac155
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-14 17:13:11 +0530

    add getMessage route

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -4,6 +4,7 @@ import protectRoute from "../middlewares/protectRoute.js";
 
 const router = Router()
 
+router.route("/:id").get(protectRoute, getMessage);
 router.route("/send/:id").post(protectRoute, sendMessage);
 
 export default router;
