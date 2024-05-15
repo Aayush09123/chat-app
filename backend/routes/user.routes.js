7dc001ffe242148a7f222b8accd7a9de566e2ffd
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-15 12:11:28 +0530

    import getUsersForSidebar controller in user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import protectRoute from "../middlewares/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/").get(protectRoute);
+router.route("/").get(protectRoute, getUsersForSidebar);
 
 export default router;
