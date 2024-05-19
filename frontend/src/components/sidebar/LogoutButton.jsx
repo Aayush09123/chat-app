2f9daa3be101f287812862f2137802c6d678032c
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 10:02:55 +0530

    bug fix in LogoutButton component

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { BiLogout } from "react-icons/bi";
+import { BiLogOut } from "react-icons/bi";
 
 const LogoutButton = () => {
     return (
         <div className='mt-auto'>
-            <BiLogout className="w-6 h-6 text-white cursor-pointer" />
+            <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
         </div>
     )
 }
