28b7a4d57bcf0dd421969d8c83eb6c4582e4e458
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 10:15:34 +0530

    add MessageContainer component

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const MessageContainer = () => {
+    return (
+        <>
+            <div className='bg-slate-500 px-4 py-2 mb-2'>
+                <span className='label-text'>To:</span>{" "}
+                <span className='text-gray-900 font-bold'>John Doe</span>
+            </div>
+        </>
+    )
+}
+
+export default MessageContainer;
