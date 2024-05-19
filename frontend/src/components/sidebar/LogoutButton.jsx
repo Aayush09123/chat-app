97966b66c36b32ef5295d6120073f3735e91cee9
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 09:48:52 +0530

    add LogoutButton component

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { BiLogout } from "react-icons/bi";
+
+const Logoutbutton = () => {
+    return (
+        <div className='mt-auto'>
+            <BiLogout className="w-6 h-6 text-white cursor-pointer" />
+        </div>
+    )
+}
+
+export default Logoutbutton;
