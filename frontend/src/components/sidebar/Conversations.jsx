d38aab5e95aff603c65f58044e282c172b12858b
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 09:44:04 +0530

    add Conversations component to show all Conversation

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Conversation from './Conversation';
+
+const Conversations = () => {
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            <Conversation />
+        </div>
+    )
+}
+
+export default Conversations;
