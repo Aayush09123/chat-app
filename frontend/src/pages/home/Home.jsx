d5cb1a70e421e1d9b1b1dfe2f6d3b1caf2f4e5d5
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 09:54:38 +0530

    add Home page

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+const Home = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Home
