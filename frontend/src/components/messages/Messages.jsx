4a8ca2b286538f87692ac8c172a584dba43a6a12
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-19 10:25:41 +0530

    add Messages component to render all Message

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Message from './Message';
+
+const Messages = () => {
+    return (
+        <div className='px-4 flex-1 overflow-auto'>
+            <Message />
+        </div>
+    )
+}
+
+export default Messages;
