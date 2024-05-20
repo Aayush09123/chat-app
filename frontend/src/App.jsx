eff4d6a44dc579170f4597f551cf59e31af33213
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-20 14:54:17 +0530

    define routes for different components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './pages/signup/Signup';
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   )
 }
