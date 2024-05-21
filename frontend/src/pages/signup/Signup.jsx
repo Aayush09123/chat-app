3ae881ace88d6ef20503535bf842f273072140e9
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-21 13:08:58 +0530

    add Link tag to signup component

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import GenderCheckbox from './GenderCheckbox';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
@@ -42,9 +43,9 @@ const Signup = () => {
 
                     <GenderCheckbox />
 
-                    <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    <Link to="/login" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                         Already have an account?
-                    </a>
+                    </Link>
 
                     <div>
                         <button className='btn btn-block btn-sm mt-4 border border-slate-700'>Sign Up</button>
