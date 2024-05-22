d75d724c5bf98101b6496b1fc256fed1ac3671ad
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-22 14:13:12 +0530

    maintaining the states of username and password

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -11,19 +18,31 @@ const Login = () => {
                     <span className='text-yellow-600'>ChatApp</span>
                 </h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Username</span>
                         </label>
-                        <input type="text" placeholder='Enter username' className='w-full input input-bordered h-10' />
+                        <input
+                            type="text"
+                            placeholder='Enter username'
+                            className='w-full input input-bordered h-10'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
 
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Password</span>
                         </label>
-                        <input type="password" placeholder='Enter password' className='w-full input input-bordered h-10' />
+                        <input
+                            type="password"
+                            placeholder='Enter password'
+                            className='w-full input input-bordered h-10'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
 
                     <Link to="/signup" className='text-sm hover:underline hover:text-blue-900 mt-4 inline-block'>
