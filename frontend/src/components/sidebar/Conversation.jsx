34fe24f795d7a032e5394a88b0d3dfc41647aa40
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-23 14:47:46 +0530

    render name and all other stuff dynamically in Conversation component

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
 
-const Conversation = () => {
+const Conversation = ({ conversation, lastIndex, emoji }) => {
     return (
         <>
             <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
                 <div className='avatar online'>
                     <div className='w-12 rounded-full'>
-                        <img src="" alt="user avatar" />
+                        <img src={conversation.profilePic} alt="user avatar" />
                     </div>
                 </div>
 
                 <div className='flex flex-col flex-1'>
                     <div className='flex gap-3 justify-between'>
                         <p className='font-bold text-gray-200'>
-                            John Doe
+                            {conversation.fullName}
                         </p>
-                        <span className='text-xl'>🤷‍♀️</span>
+                        <span className='text-xl'>{emoji}</span>
                     </div>
                 </div>
             </div>
 
-            <div className='divider my-0 py-0 h-1'></div>
+            {!lastIndex && <div className='divider my-0 py-0 h-1'></div>}
         </>
     )
 }
 
-export default Conversation
+export default Conversation;
