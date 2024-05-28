75a1904191e1dc7ff2550a2e2eef3356e1d18367
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-28 14:11:59 +0530

    import app and server in index file

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,12 @@ import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
 import connectDB from "./db/index.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 7000;
 
-const app = express();
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads
 app.use(cookieParser())
@@ -20,7 +20,7 @@ app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
 
 connectDB().then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server is listening on port: ${port}`);
     });
 }).catch((err) => {
