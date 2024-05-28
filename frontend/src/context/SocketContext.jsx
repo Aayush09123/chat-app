e2908c5d9c54dd861ca44b3251d14b0dc1472d1d
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-05-28 15:09:58 +0530

    getOnlineUsers from socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,15 +16,26 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:8000")
+            const socket = io("http://localhost:8000", {
+                query: {
+                    userId: authUser._id
+                }
+            })
             setSocket(socket);
+
+            // socket.on() is used to listen to the events and can be used both on client and server side
+            socket.on("getOnlineUsers", (users) => {
+                setOnlineUsers(users);
+            });
+
+            return () => socket.close();
         } else {
             if (socket) {
                 socket.close();
                 setSocket(null);
             }
         }
-    }, []);
+    }, [authUser]);
 
     return (
         <SocketContext.Provider value={{ socket, onlineUsers }}>
